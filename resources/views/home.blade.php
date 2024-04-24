1f90d3765fdc76ff19798448cac40ff9b8351fb8
Lucas Oliveira <lucas.as.oliveira@outlook.com>
2024-04-24 16:02:49 -0300

    Ajustando o responsivo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container-fluid d-flex flex-column gap-5 px-4 px-lg-5 py-5" style="background: #1E1E1E">
 
-        <div class="d-flex justify-content-between align-items-center ">
+        <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center gap-4 gap-lg-0">
             <div class="d-flex gap-2 align-items-center">
                 <img src="assets/images/icon-park-outline_movie.svg" alt="Ícone de filme">
                 <h1 class="text-white m-0">Catálogo de filmes</h1>
