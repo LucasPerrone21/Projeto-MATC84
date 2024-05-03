abfcb163eb32b4fc94d6517c6049148c03896d99
Helen de Freitas <82972511+heleafreitas@users.noreply.github.com>
2024-05-03 18:03:32 -0300

    criação de MVC dos filmes

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,12 @@ Route::get('/administrador', function () {
     return view('home');
 });
 
-Route::get('/cadastrar-filme', function () {
-    return view('cadastrarFilme');
-});
+
+Route::get('/cadastrar-filme', 'App\Http\Controllers\MovieController@showCreateMovieForm');
 
 Route::get('/cadastro', 'App\Http\Controllers\RegisterController@showRegistrationForm');
 Route::post('/cadastroUser', 'App\Http\Controllers\RegisterController@register');
+
 Route::get('/login', 'App\Http\Controllers\LoginController@showLoginForm');
 
 
