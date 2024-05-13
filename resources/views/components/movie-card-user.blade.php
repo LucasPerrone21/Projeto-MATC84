455e9f281c89278ede97d36730ecb9ff41d41046
Luiz Cavalcanti <luizdantas.cavalcanti@gmail.com>
2024-05-13 19:39:48 -0300

    Adiciona filmes alugados ao front-end

--- resources/views/components/movie-card-user.blade.php
+++ resources/views/components/movie-card-user.blade.php
@@ -1,20 +1,20 @@
 <div class="card rounded" style="width: 24rem;">
-  <?php 
-    header("Content-type: ".$movie->image_type);
-    echo '<img src="data:'.$movie->image_type.';base64,'.base64_encode($movie->image) .'" class="card-img-top" alt="Imagem da Capa do filme">'
+    <?php
+header("Content-type: " . $movie->image_type);
+echo '<img src="data:' . $movie->image_type . ';base64,' . base64_encode($movie->image) . '" class="card-img-top" alt="Imagem da Capa do filme">'
   ?>
 
     <div class="card-body">
-      <h5 class="card-title">{{ $movie->title }}</h5>
+        <h5 class="card-title">{{ $movie->title }}</h5>
 
-      <h6 class="card-subtitle mb-2 text-muted">{{ $movie->gender_movie }}</h6>
+        <h6 class="card-subtitle mb-2 text-muted">{{ $movie->gender_movie }}</h6>
 
-      <p class="card-text">
-        {{ $movie->description }}
-      </p>
+        <p class="card-text">
+            {{ $movie->description }}
+        </p>
 
-      <div class="d-flex gap-3">
-          <a href="#" class="btn btn-primary" style="background: #4248F2 !important">Alugar</a>
-      </div>
+        <div class="d-flex gap-3">
+            <a href="#" class="btn btn-primary" style="background: #4248F2 !important">Alugar</a>
+        </div>
     </div>
 </div>
