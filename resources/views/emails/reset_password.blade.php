9b7cdae6c42f28198ab91c1678e95c484f309dce
Sérgio Brito <mariosbps@ufba.br>
2024-05-17 18:53:01 -0300

    Personalização do email de redefinição

---
+++ resources/views/emails/reset_password.blade.php
@@ -0,0 +1,17 @@
+@component('mail::message')
+<div style="text-align: center;">
+    <img src="{{ asset('assets/images/movie_club_logo.svg') }}" width="150px" height="31px" alt="Logo do Movie Club">
+</div>
+
+# Redefinição de Senha
+
+Olá,
+
+Você está recebendo este email porque recebemos um pedido de redefinição de senha para a sua conta do The Movie Club.
+
+@component('mail::button', ['url' => $url])
+Redefinir Senha
+@endcomponent
+
+Se você não solicitou uma redefinição de senha, ignore este email.
+@endcomponent
