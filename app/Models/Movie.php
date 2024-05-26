928bad6fd1e888e75d183680a2c6b57fdc19414c
Luiz Cavalcanti <luizcdc@ufba.br>
2024-05-26 15:17:27 -0300

    Adiciona avaliações ao card de filme

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -21,5 +21,26 @@ class Movie extends Model
     {
         return $this->belongsToMany(User::class, 'current_rent_movie_user_link', 'movie_id', 'user_id');
     }
+
+    private function user_ratings()
+    {
+        return $this->belongsToMany(User::class, 'past_rent_movie_user_link', 'movie_id', 'user_id')->withPivot('rating');
+    }
+
+    public function rating()
+    {
+        // cast down to integer
+        return $this->user_ratings()->avg('rating');
+    }
+
+    public function has_ratings()
+    {
+        return $this->user_ratings()->count() > 0;
+    }
+
+    public function count_ratings()
+    {
+        return $this->user_ratings()->count();
+    }
 }
 
